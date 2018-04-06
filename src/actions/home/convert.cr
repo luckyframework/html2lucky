093bc6b83c7407f87d5ad2dcd6c0501d57e77f9a
Yousuf Jukaku <yjukaku@levelsetsolutions.com>
2018-04-06 15:13:18 -0400

    Add header to prevent chrome from marking response as XSS
    Closes #4

--- src/actions/home/convert.cr
+++ src/actions/home/convert.cr
@@ -4,6 +4,7 @@ class Home::Convert < BrowserAction
   param input : String = ""
 
   post "/" do
+    context.response.headers["X-XSS-Protection"] = "0"
     output = HTML2Lucky::Converter.new(input).convert
     render Home::IndexPage, output: output, input: input
   end
