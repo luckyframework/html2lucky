126f698171cd24c96936b2d92f0314c85449ed67
Paul Smith <paulcsmith0218@gmail.com>
2018-07-06 14:04:14 -0400

    Single line tag

--- src/tags/single_line_tag.cr
+++ src/tags/single_line_tag.cr
@@ -2,6 +2,8 @@ require "myhtml"
 
 class HTML2Lucky::SingleLineTag < HTML2Lucky::Tag
   def print_io(io)
-    io
+    io << padding + method_name.to_s + " "
+    io << wrap_quotes(squish(tag.children.first.tag_text))
+    io << ", " + attr_parameters.join(", ") if attr_parameters.any?
   end
 end
