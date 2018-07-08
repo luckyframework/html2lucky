d91bdb26a90f6f0ed9572664f0efec5e0cfa4533
Edward Loveall <edward@edwardloveall.com>
2018-07-08 18:54:35 -0400

    Add a comma between content and custom tag

--- src/tags/single_line_tag.cr
+++ src/tags/single_line_tag.cr
@@ -5,9 +5,10 @@ class HTML2Lucky::SingleLineTag < HTML2Lucky::Tag
     @content = wrap_quotes(squish(tag.children.first.tag_text))
     @attributes = attr_parameters.join(", ")
     io << padding
-    io << [[method_for, content].join(" "), attributes].
-      reject { |x| x.empty? }.
-      join(", ")
+    io << method_for
+    io << "," if custom_tag?
+    io << " "
+    io << [content, attributes].reject { |x| x.empty? }.join(", ")
     io << "\n"
   end
 end
