7c5a0a9a7c3037fcb73f3299feb6c5fbb955ddf3
Edward Loveall <edward@edwardloveall.com>
2018-07-09 19:04:57 -0400

    Refactor block start/end

--- src/tags/tag_with_children.cr
+++ src/tags/tag_with_children.cr
@@ -1,6 +1,9 @@
 require "myhtml"
 
 class HTML2Lucky::TagWithChildren < HTML2Lucky::Tag
+  BLOCK_START = " do\n"
+  BLOCK_END = "end\n"
+
   def print_io(io)
     io << padding
     io << method_name
@@ -8,14 +11,13 @@ class HTML2Lucky::TagWithChildren < HTML2Lucky::Tag
       io << method_joiner
       io << attr_text
     end
-    io << " do\n"
+    io << BLOCK_START
     children_tags = tag.children.to_a
     children_tags.shift if empty_text_tag?(children_tags.first)
     children_tags.pop if empty_text_tag?(children_tags.last)
     children_tags.each do |child_tag|
       TagFactory.new(child_tag, depth + 1).build.print_io(io)
     end
-    io << padding + "end"
-    io << "\n"
+    io << padding + BLOCK_END
   end
 end
